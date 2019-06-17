9a2b61d398a31f5e454ed4eb0185996269a78eb4
RonHovard <efremmof@yandex.ru>
2019-06-17 14:27:00 +0300

    После 5 урока и выполнения ДЗ(самостоятельно)

--- less01.py
+++ less01.py
@@ -5,7 +5,40 @@ import os
 import shutil
 import sys
 
-import psutil    # стороний модуль
+import psutil  #сторонний модуль
+
+
+def duplicate_file(filename):
+    if os.path.isfile(filename):
+        newfile = filename + '.dupl'
+        shutil.copy(filename, newfile) #копируем файл
+        if os.path.exists(newfile):
+            print("Был создан файл: ", newfile)
+            return True
+        else:
+            print("Возникли проблемы при дублировании")
+            return False    
+
+def sys_info():
+    print("Имя текущей директории:", os.getcwd())
+    print("Платформа ОС:", sys.platform)
+    print("Кодировка файловой системы:", sys.getfilesystemencoding())
+    print("Логин пользователя системы:", os.getlogin())
+    print("Количество CPU:", psutil.cpu_count())    
+
+def remove_dupl(dirname):
+    file_list = os.listdir(dirname)
+    #i = 0
+    #while i < len(file_list):
+    count_remote_dupl = 0
+    for f in file_list:
+        fullname = os.path.join(dirname, f)
+        if fullname.endswith('.dupl'):
+            os.remove(fullname)
+            count_remote_dupl += 1
+    return print("Было удалено дубликатов: ", count_remote_dupl)        
+        #i += 1    
+
 
 # Комментарий
 
@@ -27,45 +60,31 @@ while answer != 'q':
         print(" [3] - выведу список процессов")
         print(" [4] - продублирую файлы в текущей директории")
         print(" [5] - продублирую указанный файл")
-        print(" [6] - удалю дубликаты")
+        print(" [6] - удалю дубликаты в указанной директории")
         do = int(input("Укажите номер действия: "))
         
         if do == 1:
             print(os.listdir())
         elif do == 2:
-            print("Имя текущей директории:", os.getcwd())
-            print("Платформа ОС:", sys.platform)
-            print("Кодировка файловой системы:", sys.getfilesystemencoding())
-            print("Логин пользователя системы:", os.getlogin())
-            print("Количество CPU:", psutil.cpu_count())
+            sys_info()
         elif do == 3:
             print(psutil.pids())
         elif do == 4:
-            print("Продублировал файлы в текущей директории")
+            print("Сделано!")
             file_list = os.listdir()
             i = 0
             while i < len(file_list):
                 # Необходимо выполнить проверку isfile, т.к. при попытке копирования директории будет возникать ошибка
-                if os.path.isfile(file_list[i]):
-                    shutil.copy(file_list[i], file_list[i] + '.dupl')
-                # Чтобы цикл имел возможность завершиться нужно изменять переменную цикла        
+                duplicate_file(file_list[i])
                 i += 1
         elif do == 5:
             print("Дублированние указанного файла ")
             filename = input("Укажите имя файла: ")
-            if os.path.isfile(filename):
-                newfile = filename + '.dupl'
-                shutil.copy(filename, newfile)
+            duplicate_file(filename)
         elif do == 6:
             print("Удаление дубликатов в директории ")
             dirname = input("Укажите имя директории: ")
-            file_list = os.listdir(dirname)
-            i = 0
-            while i < len(file_list):
-                fullname = os.path.join(dirname, file_list[i])
-                if fullname.endswith('.dupl'):
-                    os.remove(fullname)
-                i += 1    
+            remove_dupl(dirname)
                             
         else:
             pass
